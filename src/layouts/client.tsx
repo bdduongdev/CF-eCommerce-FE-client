9b0c7c4a35b8d753d9b831c5af7813465018ce8d
quangnd05 <quangvip13102005@gmail.com>
2025-06-04 10:16:07 +0700

    scroll to top

--- src/layouts/client.tsx
+++ src/layouts/client.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
+import ScrollToTop from "./ScrollToTop";
 
 
 const ClientLayout = () => {
   return (
     <>
+        <ScrollToTop />
         <Header />
         <Outlet />
         <Footer />
